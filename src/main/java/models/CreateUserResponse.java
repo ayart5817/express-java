8c76cf34a15ef612dfda27be0922998e564e6142
a.khusnutdinov <a.khusnutdinov@zebrains.team>
2026-01-04 10:34:30 +0300

    API Tests midle Lecture

---
+++ src/main/java/models/CreateUserResponse.java
@@ -0,0 +1,23 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserResponse extends BaseModel {
+    private int id;
+    private String userName;
+    private String password;
+    private String name;
+    private String role;
+    private List<String > accounts;
+
+
+}
---
+++ src/main/java/specs/ResponseSpecs.java
@@ -0,0 +1,35 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+
+public class ResponseSpecs {
+    private ResponseSpecs() {
+    }
+
+    private static ResponseSpecBuilder defaultResponseBuilder() {
+        return new ResponseSpecBuilder();
+
+    }
+
+    public static ResponseSpecification entityWasCreated() {
+        return defaultResponseBuilder()
+                .expectStatusCode(HttpStatus.SC_CREATED)
+                .build();
+    }
+    public static ResponseSpecification requestReturnsOK() {
+        return defaultResponseBuilder()
+                .expectStatusCode(HttpStatus.SC_OK)
+                .build();
+    }
+    public static ResponseSpecification requestReturnsBadRequest(String errorKay, String errorValue ) {
+        return defaultResponseBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .expectBody(errorKay, Matchers.equalTo((errorValue)))
+                .build();
+    }
+}
+
+
